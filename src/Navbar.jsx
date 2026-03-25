acee1cd24af56509a398b85b21fe47b0b70c9383
Mimi <idleroll@protonmail.com>
2026-03-25 20:01:53 +1100

    Fix app and navbar width
    - Fix horizontal scrollbar issue by changing body and root width
    - Fix Navbar styling, it now spans the entire width of the screen

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -6,15 +6,17 @@ import { CartContext } from "./Contexts";
 const Navbar = () => {
   const { products } = useContext(CartContext);
   return (
-    <nav className={styles["navbar"]}>
-      <Link to="/shop">Shop all</Link>
-      <Link to="/" className={`logo ${styles["main-link"]}`}>
-        Fables
-      </Link>
-      <Link to="/cart">
-        Cart {products.length > 0 && `(${products.length})`}
-      </Link>
-    </nav>
+    <div className={styles["navbar-container"]}>
+      <nav className={styles["navbar"]}>
+        <Link to="/shop">Shop all</Link>
+        <Link to="/" className={`logo ${styles["main-link"]}`}>
+          Fables
+        </Link>
+        <Link to="/cart">
+          Cart {products.length > 0 && `(${products.length})`}
+        </Link>
+      </nav>
+    </div>
   );
 };
 
